731843bdae02df60cb1d9c3891cc13e10158d368
GabLux <gabrielelucchetti@live.it>
2023-11-23 21:24:50 +0100

    esercizio numeri pari giusto

--- numeriPari.js
+++ numeriPari.js
@@ -2,15 +2,18 @@ function filtraNumeriPari(listaDiNumeri) {
 
     for (let index = 0; index < listaDiNumeri.length; index++) {
 
-        if (listaDiNumeri[index] % 2 == 0){
-            console.log("Il "+ listaDiNumeri[index]+" e'pari")
-        } else{
-            console.log("Il "+ listaDiNumeri[index]+" e'dispari")
-        }
-        
+        if (listaDiNumeri[index] % 2 == 0) {
+            return true;
+        } else { return false; }
+
+
+
     }
+    return listaDiNumeri % 2 ==0
 }
 
 
-let numeri=filtraNumeriPari([1,2,3,4,5,6,7,8,9,10])
+let numeri = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+console.log(numeri.filter(filtraNumeriPari))
 
